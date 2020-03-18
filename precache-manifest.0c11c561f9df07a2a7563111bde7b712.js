c1b512bdc0b8035ec69be3d7a12923eeb033b884
Rahavee <rmanian@uvm.edu>
2020-03-18 14:17:09 -0400

    Updates

---
+++ precache-manifest.0c11c561f9df07a2a7563111bde7b712.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553e55db4bbc1e97cf17e327be36c628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "563e4a653626cbc348c5",
+    "url": "/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "c02bca438aa1e823f641",
+    "url": "/static/css/main.4d13421a.chunk.css"
+  },
+  {
+    "revision": "563e4a653626cbc348c5",
+    "url": "/static/js/2.db890b83.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.db890b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02bca438aa1e823f641",
+    "url": "/static/js/main.26678d18.chunk.js"
+  },
+  {
+    "revision": "030a43da4104d2cef268",
+    "url": "/static/js/runtime-main.160dba43.js"
+  },
+  {
+    "revision": "71b433a20ab653deaef218c2154c3029",
+    "url": "/static/media/Etch.71b433a2.jpg"
+  },
+  {
+    "revision": "d8b87e8f8e92df476058659f824edb31",
+    "url": "/static/media/Snackman.d8b87e8f.jpg"
+  },
+  {
+    "revision": "9d35e86d68467828bc032e28a44e4b4e",
+    "url": "/static/media/me.9d35e86d.jpg"
+  }
+]);
